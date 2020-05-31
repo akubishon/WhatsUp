8fa1e1df6f80247af472ba3579a50d3a084ecf02
akubishon <info.enesbaran@gmail.com>
2020-05-31 07:06:04 +0200

    login and register activities created

--- app/src/main/java/com/example/whatsup/MainActivity.kt
+++ app/src/main/java/com/example/whatsup/MainActivity.kt
@@ -2,12 +2,9 @@ package com.example.whatsup
 
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayout
 import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 import com.example.whatsup.ui.main.SectionsPagerAdapter
 
 class MainActivity : AppCompatActivity() {
@@ -20,11 +17,6 @@ class MainActivity : AppCompatActivity() {
         viewPager.adapter = sectionsPagerAdapter
         val tabs: TabLayout = findViewById(R.id.tabs)
         tabs.setupWithViewPager(viewPager)
-        val fab: FloatingActionButton = findViewById(R.id.fab)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
     }
 }
