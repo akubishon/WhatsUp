5b92c26ea18f69dcfe284941fba557f6a8f043cd
akubishon <info.enesbaran@gmail.com>
2020-06-20 21:28:01 +0200

    bro everything works

--- app/src/main/java/com/example/whatsup/MainActivity.kt
+++ app/src/main/java/com/example/whatsup/MainActivity.kt
@@ -1,22 +1,78 @@
 package com.example.whatsup
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.widget.Toolbar
 import com.google.android.material.tabs.TabLayout
 import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
-import com.example.whatsup.ui.main.SectionsPagerAdapter
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.example.whatsup.Fragments.ChatsFragment
+import com.example.whatsup.Fragments.SearchFragment
+import com.example.whatsup.Fragments.SettingsFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
-        val viewPager: ViewPager = findViewById(R.id.view_pager)
-        viewPager.adapter = sectionsPagerAdapter
-        val tabs: TabLayout = findViewById(R.id.tabs)
-        tabs.setupWithViewPager(viewPager)
+        setSupportActionBar(toolbar_main)
 
+        val toolbar: Toolbar = findViewById(R.id.toolbar_main)
+        setSupportActionBar(toolbar)
+        supportActionBar!!.title = ""
+        val tabLayout: TabLayout = findViewById(R.id.tab_layout)
+        val viewPager: ViewPager = findViewById(R.id.view_pager)
+        val viewPagerAdapter = ViewPagerAdapter(supportFragmentManager)
+        viewPagerAdapter.addFragment(ChatsFragment(), "chats")
+        viewPagerAdapter.addFragment(SearchFragment(), "search")
+        viewPagerAdapter.addFragment(SettingsFragment(), "Settings")
+
+        viewPager.adapter = viewPagerAdapter
+        tabLayout.setupWithViewPager(viewPager)
     }
-}
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+
+
+    internal class ViewPagerAdapter(fragmentManager: FragmentManager):
+        FragmentPagerAdapter(fragmentManager) {
+
+
+            private val fragments: ArrayList<Fragment>
+            private val titles: ArrayList<String>
+
+            init
+            {
+                fragments = ArrayList<Fragment>()
+                titles = ArrayList<String>()
+            }
+            override fun getItem(position: Int): Fragment {
+                return fragments[position]
+
+            }
+
+            override fun getCount(): Int {
+            return fragments.size
+            }
+
+            fun addFragment(fragment: Fragment, title: String){
+            fragments.add(fragment)
+            titles.add(title)
+            }
+
+            override fun getPageTitle(i: Int): CharSequence? {
+            return titles[i]
+            }
+        }
+    }
